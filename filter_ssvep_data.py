e40dd0c4a5f77d9034f1b8bb4bea055ffded39ec
andres-s-d <asegura@ndigital.com>
2024-03-12 22:14:12 -0400

    starting part 1 and 2

---
+++ filter_ssvep_data.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 11 18:41:31 2024
+
+@author: asegura
+"""
+
+from pylab import *
+from scipy.signal import firwin, lfilter, filtfilt,freqz
+import matplotlib.pyplot as plt
+
+def make_bandpass_filter(low_cutoff,high_cutoff,filter_type,filter_order,fs):
+    
+    if filter_type==None: filter_type='hann'
+    fNQ = fs/2                                     #Compute the Niqyst rate
+    taps_number = filter_order                     # Define the filter order
+    #Wn = [low_cutoff/fNQ ,high_cutoff/fNQ]         # ... and specify the cutoff frequency normalized to Nyquest rate
+    Wn = [low_cutoff ,high_cutoff]         # ... and specify the cutoff frequency normalized to Nyquest rate
+    #filter_coefficients  = firwin(taps_number, Wn, window=filter_type, pass_zero='bandpass')              # ... build lowpass FIR filter,
+    filter_coefficients  = firwin(taps_number, Wn, window=filter_type, pass_zero=False,fs=fs)              # ... build lowpass FIR filter,
+    
+    w, h = freqz(filter_coefficients)                      #Compute the frequency response
+    
+    
+    fig, axs = plt.subplots(2)
+    axs[0].set_title('Digital filter frequency response')
+    axs[0].plot(w, 20 * np.log10(abs(h)), 'b')
+    axs[0].set_ylabel('Amplitude [dB]', color='b')
+    axs[0].set_xlabel('Frequency [rad/sample]')
+    ax2 = axs[0].twinx()
+    angles = np.unwrap(np.angle(h))
+    ax2.plot(w, angles, 'g')
+    ax2.set_ylabel('Angle (radians)', color='g')
+    ax2.grid(True)
+    ax2.axis('tight')
+    axs[1].set_title('Digital filter impulse response')
+    axs[1].plot(filter_coefficients, 'b')
+    axs[1].set_ylabel('Amplitude ', color='b')
+    axs[1].set_xlabel('Sample Number  [1/fs]')
+    axs[1].grid(True)
+    
+    
+    plt.tight_layout()
+    plt.show()
+    
+    return filter_coefficients 
+
---
+++ test_filter_ssvep_data.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+andres Segura & Tynan Gacy
+BME 6770: BCI's Lab 04
+Dr. David Jangraw
+3/11/2024
+
+This script intends to ...
+
+
+"""
+
+# Inport the necessry modules
+import os
+import sys
+import matplotlib.pyplot as plt
+import import_ssvep_data
+import filter_ssvep_data
+
+#Make sure relative path work
+cwd=os.getcwd()
+sys.path.insert(0,f"{cwd}\course_software\SsvepData\\")
+
+#Close previosly drawn plots
+plt.close('all')
+
+#Build data file string
+data_directory=f'{cwd}/course_software/SsvepData/'
+subject=1
+#data_file=f'{cwd}{data_directory}SSVEP_S{subject}.npz'
+
+
+#%% Cell1 Load the Data
+
+data=import_ssvep_data.load_ssvep_data(subject,data_directory)
+
+#%% Cell2 Design a Filter
+
+#Filter out the 15Hz signals
+low_cutoff=13
+high_cutoff=17
+filter_type='hann'
+filter_order=1000
+fs=data['fs']
+
+filter_coefficients=filter_ssvep_data.make_bandpass_filter(low_cutoff,high_cutoff,filter_type,filter_order,fs)
